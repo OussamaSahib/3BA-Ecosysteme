0bed653fd2721c247701936f420a9d1d66a688be
Oussama Sahib <195052@ecam.be>
2022-12-06 00:19:23 +0100

    Ajoutez des fichiers projet.

---
+++ Ecosysteme/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Ecosysteme;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
