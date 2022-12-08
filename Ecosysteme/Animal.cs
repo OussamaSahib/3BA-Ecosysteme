0358b80b0870e4fd9a3fa409563b3be6a01aa2cf
Oussama Sahib <195052@ecam.be>
2022-12-08 15:40:02 +0100

    Jeu (Déplacement aléatoire de l'être vivant)

--- Ecosysteme/Animal.cs
+++ Ecosysteme/Animal.cs
@@ -9,8 +9,23 @@ namespace Ecosysteme
 
         public override void Update()
         {
+            //PAS RANDOM
             //Nb de pas
-            X= X + 30;
+            int pas= 30;
+            //Pour X: Rien, Gauche, Droite
+            List<int>numbersX= new List<int>() {0, -pas, pas};
+            //Pour X: Rien, Bas, Haut
+            List<int>numbersY= new List<int>() {0, -pas, pas};
+
+            Random rnd= new Random();
+            //Pour X et Y: Choisir 1'élément aléatoire de la liste de pas
+            int randIndexX= rnd.Next(numbersX.Count);
+            int randomX= numbersX[randIndexX];
+            int randIndexY= rnd.Next(numbersY.Count);
+            int randomY= numbersY[randIndexY];
+
+            X= X +randomX;
+            Y= Y +randomY;
         }
     }
 }
