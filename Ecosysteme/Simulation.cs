1780f3e7d36d0f3f0c2d2574447b50f1c4b7b52c
Oussama Sahib <195052@ecam.be>
2022-12-10 14:53:22 +0100

    Jeu (Création de Barre d'énergie+Coeur de vie)

--- Ecosysteme/Simulation.cs
+++ Ecosysteme/Simulation.cs
@@ -19,8 +19,49 @@ namespace Ecosysteme
         {
             foreach (SimulationObjet drawable in objects)
             {
-                canvas.FillColor = drawable.Color;
+                // ÊTRE VIVANT
+                canvas.FillColor= drawable.Color;
                 canvas.FillCircle(new Point(drawable.X, drawable.Y), 25.0);
+
+
+                //BARRE D'ENERGIE
+                //Barre grise
+                canvas.FillColor= Colors.Gray;
+                canvas.FillRoundedRectangle(Convert.ToSingle(drawable.X)-37, Convert.ToSingle(drawable.Y)-70, 75, 15, 5);
+                
+                //Barre d'energie
+                canvas.FillColor = Colors.LimeGreen;
+                canvas.FillRoundedRectangle(Convert.ToSingle(drawable.X)-37, Convert.ToSingle(drawable.Y)-70, 75, 15, 5);
+             
+
+                //COEURS DE VIE
+                void Coeur(int posx, int posy)
+                {
+                    //2 cercles -->(x,y, largeur, hauteur, angle départ, angle fin, sens)
+                    canvas.FillArc(Convert.ToSingle(drawable.X)-posx, Convert.ToSingle(drawable.Y)-65+15, 15, 15, 0, 180, false);
+                    canvas.FillArc(Convert.ToSingle(drawable.X)-posx+15, Convert.ToSingle(drawable.Y)-65+15, 15, 15, 0, 180, false);
+                    //Triangle -->sens horloger(pt à pt)
+                    PathF path= new PathF();
+                    path.MoveTo(Convert.ToSingle(drawable.X)-posx, Convert.ToSingle(drawable.Y)-65+15+Convert.ToSingle(7.5));
+                    path.LineTo(Convert.ToSingle(drawable.X)-posx+15, Convert.ToSingle(drawable.Y)-65+40);
+                    path.LineTo(Convert.ToSingle(drawable.X)-posx+30, Convert.ToSingle(drawable.Y)-65+15+Convert.ToSingle(7.5));
+                    canvas.FillPath(path);
+                }
+                //COEUR DE VIE 1
+                //Coeur gris
+                canvas.FillColor= Colors.Gray;
+                Coeur(35, 35);
+                //Coeur de vie
+                canvas.FillColor= Colors.Red;
+                Coeur(35, 35);
+
+                //COEUR DE VIE 2
+                //Coeur gris
+                canvas.FillColor= Colors.Gray;
+                Coeur(0, 0);
+                //Coeur de vie
+                canvas.FillColor= Colors.Red;
+                Coeur(0, 0);
             }
         }
 
