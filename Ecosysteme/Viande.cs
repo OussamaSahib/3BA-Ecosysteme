b9e9d66cf42f67dd5a45bd1705a7637e503a1704
Oussama Sahib <195052@ecam.be>
2022-12-31 23:20:23 +0100

    Jeu (Pour Viande: Structure du code en Fonctions)

--- Ecosysteme/Viande.cs
+++ Ecosysteme/Viande.cs
@@ -5,15 +5,19 @@ namespace Ecosysteme
     public class Viande: SimulationObjet
     {
         
-        public Viande(double x, double y, int vie_viande) : base(Colors.SaddleBrown, x, y, 0,0,0, vie_viande) { }
+        public Viande(double x, double y, int vie_viande): base(Colors.SaddleBrown, x, y, 0,0,0, vie_viande) { }
+
+        public void VieViande()
+        {   //Vie_Viande qui diminue
+            if (Vie_Viande>0)
+                Vie_Viande= Vie_Viande-5;
+            else
+                Vie_Viande= 0;
+        }
 
         public override void Update()
         {
-            if (Vie_Viande>0)
-                Vie_Viande= Vie_Viande-5;
-
-            else
-                Vie_Viande=0;
+            VieViande();
         }
     }
 }
